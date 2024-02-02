e3d16a8d947955c75c5f3deee880c9f8596e7fc9
ashley <revesfideles@gmail.com>
2024-02-02 03:46:34 -0500

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    employee: "Sam",
+    streetAddress: "11 Broadway"    
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = { ...employee };
+    newEmployee[key] = value
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(newEmployee, key, value){
+     newEmployee[key] = value 
+     return newEmployee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = { ...employee }
+    delete newEmployee[key]
+    return newEmployee
+}   
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
